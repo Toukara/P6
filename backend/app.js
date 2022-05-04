7c5bf918be0e5a57982adc8b7c64d4e5ca059158
Toukara <benjamincms62@gmail.com>
2022-05-04 13:39:13 +0200

    .

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 });
 
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 5 * 60 * 1000, // 5 minutes
   max: 200, // limit each IP to 200 requests per windowMs
   message: "Too many requests, please try again later",
 });
